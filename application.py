bf232e11e4c577cb050c2b4737ab01448e61b667
Durigy <56241583+Durigy@users.noreply.github.com>
2023-04-16 23:11:19 +0100

    socket time

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 from main import app
-from main import socketio as application
+from main import socketio 
+#as application
 
 if __name__ == "__main__":
-	application.run(app)
+	socketio.run(app)
 
 
 

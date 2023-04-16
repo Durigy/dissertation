df5066bd44a8c670f9f4b1acf236bb22374e5ef0
Durigy <56241583+Durigy@users.noreply.github.com>
2023-04-16 06:41:37 +0100

    hopeful fix

--- main/__init__.py
+++ main/__init__.py
@@ -70,7 +70,7 @@ if os.environ.get("SQLALCHEMY_DATABASE_URI"):
     SQLALCHEMY_DATABASE_URI= f'mysql+pymysql://{os.environ.get("RDS_USERNAME")}:{os.environ.get("RDS_PASSWORD")}@{os.environ.get("RDS_HOSTNAME")}:{os.environ.get("RDS_PORT")}/{os.environ.get("RDS_DB_NAME")}'
 
 DEBUG = os.environ.get("DEBUG") if os.environ.get("DEBUG") else DEBUG
-REMEMBER_COOKIE_DURATION = timedelta(days = os.environ.get("REMEMBER_COOKIE_DURATION")) if os.environ.get("REMEMBER_COOKIE_DURATION") else REMEMBER_COOKIE_DURATION
+REMEMBER_COOKIE_DURATION = timedelta(days = int(os.environ.get("REMEMBER_COOKIE_DURATION"))) if os.environ.get("REMEMBER_COOKIE_DURATION") else REMEMBER_COOKIE_DURATION
 SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS") if os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS") else SQLALCHEMY_TRACK_MODIFICATIONS
 # UPLOAD_FOLDER = 
 # ALLOWED_EXTENSIONS = '' #{'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -127,7 +127,7 @@ app.config['DOCUMENT_EXTENSIONS'] = DOCUMENT_EXTENSIONS
 # 
 app.config['ALLOWED_EXTENSIONS'] = IMAGE_EXTENSIONS + DOCUMENT_EXTENSIONS
 
-print(app.config['ALLOWED_EXTENSIONS'])
+# print(app.config['ALLOWED_EXTENSIONS'])
 
 
 
